aac2ccf59b99d463691e4302cf8a4c2488b33c0b
kai <kai@simbo.com.tw>
2020-07-27 23:01:06 +0800

    Add product detail get id by route

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>This is a product page</h1>
+    <div>{{product.title}}</div>
+    <div>{{product.content}}</div>
+    <div>{{product.price}}</div>
   </div>
 </template>
 
@@ -13,7 +16,13 @@ export default {
     }
   },
   created () {
-    console.log(this.$route.params)
+    this.axios.get(`${process.env.VUE_APP_API_URL}api/${process.env.VUE_APP_UUID}/ec/product/${this.$route.params.id}`)
+      .then(res => {
+        this.product = res.data.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 }
 </script>
