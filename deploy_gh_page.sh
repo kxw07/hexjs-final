d5a6ca912beecdd4a977265df9e7f220b28345c3
kai <kai@simbo.com.tw>
2020-07-28 10:17:29 +0800

    Update deployment

--- deploy_gh_page.sh
+++ deploy_gh_page.sh
@@ -6,18 +6,11 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
+cd ~/dojo/js/hexjs
+rm -rf 20200724
+mkdir 20200724
+cp -r ~/dojo/js/hexjs-final/dist/. ./20200724/
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f master:gh-pages
+git add .
+git commit -m "Deploy 20200724"
+git push
